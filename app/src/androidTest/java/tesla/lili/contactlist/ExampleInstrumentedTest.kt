05f005c46126a14360ca3ece7869291c86131c0a
Lily Tesla <lilitesla.fox@gmail.com>
2018-11-01 14:57:23 +0300

    Initial commit

---
+++ app/src/androidTest/java/tesla/lili/contactlist/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package tesla.lili.contactlist
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("tesla.lili.contactlist", appContext.packageName)
+    }
+}
